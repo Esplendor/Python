a94072371982646aa8afc879fb4a4722dea0259e
ᛁ╵ި⸌ᛁ⸜⸍ˎ⡄ <131931306+esplendor@users.noreply.github.com>
2023-12-17 14:24:44 -0300

    portscan.py

---
+++ portscan.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket,sys
+
+argv = str(input("Type your target: "))
+
+if len(argv) <= 1:
+        print ("Use mode: ./portscan.py 10.10.0.1 or ./portscan.py https://site>
+else:
+        for port in range(1,65535):
+                mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client = mysocket.connect_ex((argv,port))
+
+                if (client == 0):
+                        print ("Port:",port,"[OPEN]")
+                        mysocket.close()
